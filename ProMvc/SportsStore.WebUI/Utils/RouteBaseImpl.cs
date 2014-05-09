9c0e3b85ead364b98289ebcc41b50c122fe2c1ed
luchunminglu <luchunminglu@gmail.com>
2014-05-09 02:16:03 -0700

    cc

---
+++ ProMvc/SportsStore.WebUI/Utils/RouteBaseImpl.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Routing;
+
+namespace SportsStore.WebUI.Utils
+{
+    public class RouteBaseImpl:RouteBase
+    {
+        /// <summary>
+        /// inbound url match
+        /// </summary>
+        /// <param name="httpContext"></param>
+        /// <returns></returns>
+        public override RouteData GetRouteData(HttpContextBase httpContext)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// outbound url generation
+        /// </summary>
+        /// <param name="requestContext"></param>
+        /// <param name="values"></param>
+        /// <returns></returns>
+        public override VirtualPathData GetVirtualPath(RequestContext requestContext, RouteValueDictionary values)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
