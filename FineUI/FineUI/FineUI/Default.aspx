ea8a121ef3edc25cbd977f3422d2e77988535ec7
luchunminglu <luchunminglu@gmail.com>
2014-12-24 09:45:14 +0800

    cc

--- FineUI/FineUI/FineUI/Default.aspx
+++ FineUI/FineUI/FineUI/Default.aspx
@@ -1,5 +1,6 @@
 ﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="FineUI.Default" %>
-<%@ Register assembly="FineUI" namespace="FineUI" tagPrefix="f" %>
+<%@ Register namespace="FineUI" assembly="FineUI" tagPrefix="f" %>
+
 <!DOCTYPE html>
 
 <html xmlns="http://www.w3.org/1999/xhtml">
@@ -10,9 +11,7 @@
 <body>
     <form id="form1" runat="server">
     <div>
-    <f:Window ID="Window1" runat="server" Title="登陆表单" IsModal="False" EnableClose="False" WindowPosition="GoldenSection" Width="350px">
-        
-    </f:Window>
+    
     </div>
     </form>
 </body>
--- FineUI/FineUI/FineUI/Properties/AssemblyInfo.cs
+++ FineUI/FineUI/FineUI/Properties/AssemblyInfo.cs
@@ -8,28 +8,28 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("FineUI")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Microsoft")]
 [assembly: AssemblyProduct("FineUI")]
-[assembly: AssemblyCopyright("版权所有(C)  2014")]
+[assembly: AssemblyCopyright("Copyright © Microsoft 2014")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
 // 将 ComVisible 设置为 false 会使此程序集中的类型
-// 对 COM 组件不可见。如果需要从 COM 访问此程序集中的某个类型，
-// 请针对该类型将 ComVisible 特性设置为 true。
+// 对 COM 组件不可见。如果需要
+// 从 COM 访问此程序集中的某个类型，请针对该类型将 ComVisible 特性设置为 true。
 [assembly: ComVisible(false)]
 
-// 如果此项目向 COM 公开，则下列 GUID 用于类型库的 ID
-[assembly: Guid("a730ae97-fd13-43e6-bf4a-2b01f1282a23")]
+// 如果此项目向 COM 公开，则下列 GUID 用于 typelib 的 ID
+[assembly: Guid("b8661a31-0f74-4706-864f-28bbad553e16")]
 
 // 程序集的版本信息由下列四个值组成:
 //
 //      主版本
 //      次版本
 //      内部版本号
-//      修订号
+//      修订版本
 //
-// 可以指定所有这些值，也可以使用“修订号”和“内部版本号”的默认值，
-// 方法是按如下所示使用“*”:
+// 可以指定所有值，也可以使用“修订号”和“内部版本号”的默认值，
+// 方法是按如下所示使用 "*":
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
