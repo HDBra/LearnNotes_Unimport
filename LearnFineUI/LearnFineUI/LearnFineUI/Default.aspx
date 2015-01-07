eb9df12f5d6e5f30861a289c71a5d35dae425a2e
luchunminglu <luchunminglu@gmail.com>
2015-01-07 08:31:18 +0800

    cc

--- LearnFineUI/LearnFineUI/LearnFineUI/Default.aspx
+++ LearnFineUI/LearnFineUI/LearnFineUI/Default.aspx
@@ -11,22 +11,7 @@
     <form id="form1" runat="server">
         <f:PageManager runat="server" />
         
-        <f:Panel Layout="VBox" BoxConfigAlign="Stretch" BoxConfigPosition="End"  BoxConfigPadding="5" BoxConfigChildMargin="0 0 5 0" runat="server">
-            <Items>
-                <f:Panel BoxFlex="1" runat="server"/>
-                <f:Panel Height="100px" runat="server"/>
-                <f:Panel BoxFlex="2" BoxMargin="0" runat="server"/>
-            </Items>
-        </f:Panel>
-        <!-- 
-            
-            BoxConfigAlign:用来控制容器子控件的尺寸，有四种取值：
-            Start：所有子控件位于父容器开始位置
-            Center:所有子控件位于父容器的中间位置
-            Stretch:所有子控件被拉伸至父容器的大小
-            StretchMax:所有子控件被拉伸至最大子控件的大小
-             -->
-
+        
     </form>
 </body>
 </html>
--- LearnFineUI/LearnFineUI/LearnFineUI/Default.aspx.cs
+++ LearnFineUI/LearnFineUI/LearnFineUI/Default.aspx.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
@@ -14,10 +15,68 @@ namespace LearnFineUI
         {
             if (!IsPostBack)
             {
-               
+                Grid1.DataSource = GetDataTable();
+                Grid1.DataBind();
             }
         }
 
+
+
+        protected DataTable GetDataTable()
+        {
+            DataTable table = new DataTable();
+            table.Columns.Add(new DataColumn("Id", typeof (int)));
+            table.Columns.Add(new DataColumn("Name", typeof (string)));
+            table.Columns.Add(new DataColumn("EntranceYear", typeof (String)));
+            table.Columns.Add(new DataColumn("AtSchool", typeof (bool)));
+            table.Columns.Add(new DataColumn("Major", typeof (string)));
+            table.Columns.Add(new DataColumn("Group", typeof (int)));
+            table.Columns.Add(new DataColumn("Gender", typeof (int)));
+            table.Columns.Add(new DataColumn("LogTime", typeof (DateTime)));
+            table.Columns.Add(new DataColumn("Desc", typeof (string)));
+            table.Columns.Add(new DataColumn("Guid", typeof (Guid)));
+
+            DataRow row = table.NewRow();
+            row[0] = 101;
+            row[1] = "陈萍萍";
+            row[2] = "2000";
+            row[3] = true;
+            row[4] = "计算机应用技术";
+            row[5] = 1;
+            row[6] = 0;
+            row[7] = DateTime.Now.AddDays(-100);
+            row[8] = "陈萍萍，女，20岁，出生于中国南方的一个小山村，毕业于中国科学技术大学。";
+            row[9] = new Guid();
+            table.Rows.Add(row);
+  
+            row = table.NewRow();
+            row[0] = 102;
+            row[1] = "胡飞";
+            row[2] = "2008";
+            row[3] = false;
+            row[4] = "信息工程";
+            row[5] = 1;
+            row[6] = 1;
+            row[7] = DateTime.Now.AddDays(-90);
+            row[8] = "胡飞，男，20岁，出生于中国北方的一个小山村，毕业于南方科学技术大学。";
+            row[9] = new Guid();
+            table.Rows.Add(row);
+  
+            row = table.NewRow();
+            row[0] = 103;
+            row[1] = "金婷婷";
+            row[2] = "2001";
+            row[3] = true;
+            row[4] = "会计学";
+            row[5] = 2;
+            row[6] = 0;
+            row[7] = DateTime.Now.AddDays(-80);
+            row[8] = "金婷婷，女，28岁，出生于中国海南岛的一个小山村，毕业于中国科学技术大学。";
+            row[9] = new Guid();
+            table.Rows.Add(row);
+  
+            return table;
+        }
     }
 
 }
