7a6ca74a0f0d3ab7e7fc02352574de5d33049414
luchunminglu <luchunminglu@gmail.com>
2015-03-18 13:45:53 +0800

    cc

---
+++ WpfLearn/WpfLearn/Controls/ListView3.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using WpfLearn.Deletable;
+
+namespace WpfLearn.Controls
+{
+    /// <summary>
+    /// ListView3.xaml 的交互逻辑
+    /// </summary>
+    public partial class ListView3 : Window
+    {
+        public ListView3()
+        {
+            InitializeComponent();
+        }
+
+        private void ListView3_OnLoaded(object sender, RoutedEventArgs e)
+        {
+            LstProducts.ItemsSource = Product.GetProducts();
+            //LstProducts.View = new TileView();
+            //LstProducts.View = new GridView();
+        }
+    }
+}
---
+++ WpfLearn/WpfLearn/Deletable/TileView.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace WpfLearn.Deletable
+{
+    public class TileView:ViewBase
+    {
+        /// <summary>
+        /// 数据模板，描述如何显示数据
+        /// </summary>
+        private DataTemplate itemTemplate;
+
+        /// <summary>
+        /// 数据模板，描述如何显示一项数据
+        /// </summary>
+        public DataTemplate ItemTemplate
+        {
+            get { return itemTemplate; }
+            set { itemTemplate = value; }
+        }
+
+        /// <summary>
+        ///  样式对象，用来设定ListView本身的样式
+        /// </summary>
+        protected override object DefaultStyleKey
+        {
+            get { return new ComponentResourceKey(GetType(), "TileView"); }
+        }
+
+        /// <summary>
+        /// view中Item项的样式
+        /// </summary>
+        protected override object ItemContainerDefaultStyleKey
+        {
+            //获取type类型key指定的键的名字的资源
+            get { return new ComponentResourceKey(GetType(), "TileViewItem"); }
+        }
+    }
+}
