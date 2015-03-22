b796b43eb475454b14a9a3ce8ec97470214f8ae2
luchunminglu <luchunminglu@gmail.com>
2015-03-22 19:51:48 +0800

    cc

---
+++ WpfLearn/WpfLearn/Windows/NonrectangularWindows_Border.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WpfLearn.Windows
+{
+    /// <summary>
+    /// NonrectangularWindows_Border.xaml 的交互逻辑
+    /// </summary>
+    public partial class NonrectangularWindows_Border : Window
+    {
+        public NonrectangularWindows_Border()
+        {
+            InitializeComponent();
+        }
+
+        private bool isWiden = true;
+        private void UIElement_OnMouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            isWiden = true;
+        }
+
+        private void UIElement_OnMouseLeftButtonUp(object sender, MouseButtonEventArgs e)
+        {
+            isWiden = false;
+
+
+            Rectangle rect = (Rectangle)sender;
+            rect.ReleaseMouseCapture();
+        }
+
+        private void UIElement_OnMouseMove(object sender, MouseEventArgs e)
+        {
+            Rectangle rect = (Rectangle)sender;
+            if (isWiden)
+            {
+                rect.CaptureMouse();
+                double newWidth = e.GetPosition(this).X + 5;
+                if (newWidth > 0) this.Width = newWidth;
+            }
+        }
+    }
+}
---
+++ WpfLearn/WpfLearn/Windows/NonrectgularWindow_Path.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WpfLearn.Windows
+{
+    /// <summary>
+    /// NonrectgularWindow_Path.xaml 的交互逻辑
+    /// </summary>
+    public partial class NonrectgularWindow_Path : Window
+    {
+        public NonrectgularWindow_Path()
+        {
+            InitializeComponent();
+        }
+
+        private void UIElement_OnMouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            DragMove();
+        }
+    }
+}
