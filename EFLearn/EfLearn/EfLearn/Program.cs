e31458e5e318075e14f6d4646c833dbaa4b97d60
luchunminglu <luchunminglu@gmail.com>
2015-03-25 23:36:10 +0800

    xc

--- EFLearn/EfLearn/EfLearn/Program.cs
+++ EFLearn/EfLearn/EfLearn/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,7 +12,45 @@ namespace EfLearn
     {
         public static void Main(string[] args)
         {
-            DbContextHelper.Test();
+            using (var dbContext = new SchoolContext())
+            {
+                Student student = new Student(){StudentName = "Steve"};
+                dbContext.Students.Add(student);
+                dbContext.SaveChanges();
+            }
         }
     }
+
+
+    public class Student
+    {
+        public int StudentID { get; set; }
+        public string StudentName { get; set; }
+        public DateTime? DateOfBirth { get; set; }
+        public byte[] Photo { get; set; }
+        public decimal Height { get; set; }
+        public float Weight { get; set; }
+        public Standard Standard
+        {
+            get;
+            set;
+        }
+    }
+
+    public class Standard
+    {
+        public int StandardId { get; set; }
+        public string StandardName { get; set; }
+        public ICollection<Student> Students { get; set; }
+    }
+
+    public class SchoolContext : DbContext
+    {
+        public DbSet<Student> Students { get; set; }
+        public DbSet<Standard> Standards { get; set; }
+    }
+
+
+
+
 }
--- EFLearn/EfLearn/EfLearn/Utils/DbContextHelper.cs
+++ EFLearn/EfLearn/EfLearn/Utils/DbContextHelper.cs
@@ -22,34 +22,5 @@ namespace EfLearn.Utils
         }
 
 
-        public static void Test()
-        {
-            using (var context = new SchoolDBEntities())
-            {
-                var l2EQuery = context.Students.Where(s => s.StudentID == 5);
-                var student = l2EQuery.FirstOrDefault();
-                Console.WriteLine(student==null?"":student.StudentName);
-            }
-
-            using (var context = new SchoolDBEntities())
-            {
-                context.Configuration.AutoDetectChangesEnabled = true;
-                var studentList = context.Students.ToList<Student>();
-
-                //Perform create operation
-                context.Students.Add(new Student() { StudentName = "New Student" });
-
-                //Perform Update operation
-                Student studentToUpdate = studentList.Where(s => s.StudentName == "student1").FirstOrDefault<Student>();
-                studentToUpdate.StudentName = "Edited student1";
-
-                //Perform delete operation
-                context.Students.Remove(studentList.ElementAt<Student>(0));
-
-                //Execute Inser, Update & Delete queries in the database
-                context.SaveChanges();
-
-            }
-        }
     }
 }
