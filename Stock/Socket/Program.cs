16ef58f33f4d2de409d31cacfcf2ad12c97a1145
luchunminglu <luchunminglu@gmail.com>
2015-06-12 16:47:30 +0800

    vv

--- Stock/Socket/Program.cs
+++ Stock/Socket/Program.cs
@@ -91,7 +91,7 @@ namespace Socket
         [DllImport("user32.dll", CharSet = CharSet.Auto)]
         private static extern System.IntPtr GetForegroundWindow();
 
-        static string[] Stocks = { "sh000001","sh601288"};
+        static string[] Stocks = { "sh000001","sh601398"};
         const string ApiNet = @"http://hq.sinajs.cn/list=";
     }
 }
