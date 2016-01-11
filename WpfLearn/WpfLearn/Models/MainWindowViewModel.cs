d3e52db634be50fe916f10f1d22c3424970e072d
luchunminglu <luchunminglu@gmail.com>
2016-01-11 09:18:31 +0800

    cc

---
+++ WpfLearn/WpfLearn/Models/MainWindowViewModel.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfLearn.Models
+{
+    public class MainWindowViewModel:INotifyPropertyChanged
+    {
+        private PersonModel _model;
+        private string _fullName;
+
+        public MainWindowViewModel()
+        {
+            _model = new PersonModel()
+            {
+                FirstName = "Tom",
+                LastName = "James"
+            };
+
+            FullName = string.Format("{0} {1}", _model.FirstName, _model.LastName);
+        }
+
+
+        public string FirstName
+        {
+            get { return _model.FirstName; }
+            set
+            {
+                _model.FirstName = value;
+                FullName = string.Format("{0} {1}",_model.FirstName, _model.LastName);
+                OnPropertyChanged("FirstName");
+            }
+        }
+
+        public string LastName
+        {
+            get { return _model.LastName; }
+            set
+            {
+                _model.LastName = value;
+                FullName = string.Format("{0} {1}",_model.FirstName, _model.LastName);
+                OnPropertyChanged("LastName");
+            }
+        }
+
+        public string FullName
+        {
+            get { return _fullName;}
+            set
+            {
+                _fullName = value;
+                OnPropertyChanged("FullName");
+            }
+        }
+
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+
+        public void OnPropertyChanged(string propertyName)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+
+    }
+}
---
+++ WpfLearn/WpfLearn/Models/PersonModel.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfLearn.Models
+{
+    public class PersonModel:INotifyPropertyChanged
+    {
+
+        private string _firstName;
+        private string _lastName;
+
+        public string FirstName
+        {
+            get { return _firstName;}
+            set
+            {
+                _firstName = value;
+                OnPropertyChanged("FirstName");
+            }
+        }
+
+        public string LastName
+        {
+            get { return _lastName;}
+            set
+            {
+                _lastName = value;
+                OnPropertyChanged("LastName");
+            }
+        }
+
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+
+        public void OnPropertyChanged(string propertyName)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this,new PropertyChangedEventArgs(propertyName));
+            }
+        }
+    }
+}
