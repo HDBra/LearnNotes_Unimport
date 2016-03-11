cc7235de2a6f33e2d00689b364b518cc3382d456
luchunminglu <luchunminglu@gmail.com>
2016-03-11 23:19:56 +0800

    cc

---
+++ ProWebApi2/MvcWebApi2UI/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MvcWebApi2UI.Global" Language="C#" %>
---
+++ ProWebApi2/ProWebApi2/Utils/ApiControllerHelper.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Web;
+using System.Web.Http;
+
+namespace ProWebApi2.Utils
+{
+    /// <summary>
+    /// ApiController帮助类
+    /// </summary>
+    public static class ApiControllerHelper
+    {
+
+        /// <summary>
+        /// 获取HttpRequestMessage,它包含了所有的请求信息
+        /// </summary>
+        /// <param name="controller"></param>
+        /// <returns></returns>
+        public static HttpRequestMessage GetRequest(ApiController controller)
+        {
+            return controller.Request;
+        }
+
+
+    }
+}
