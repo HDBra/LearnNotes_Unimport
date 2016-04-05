3bae571bfe78f72096d4a0c85a8151b932b8dab5
luchunminglu <luchunminglu@gmail.com>
2016-04-05 08:54:56 +0800

    cc

---
+++ MatlabUtils/samples.m
@@ -0,0 +1,19 @@
+
+% Ñ­»·ÑùÀı
+
+for i = 1:50
+ display(i);   
+end
+
+
+% ifÅĞ¶ÏÓï¾ä
+n = 0.5;
+
+if n<0.5
+    disp('n is smaller than o.5');
+elseif n>0.5
+    disp('n is bigger than 0.5');
+else
+    disp('n is 0.5');
+end
+
