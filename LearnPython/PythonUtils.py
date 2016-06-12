c7d74595eeda28da75345903a670eec029bd874e
luchunminglu <luchunminglu@gmail.com>
2016-06-12 09:03:04 +0800

    cc

--- LearnPython/PythonUtils.py
+++ LearnPython/PythonUtils.py
@@ -1,3 +1,10 @@
 if __name__ == "__main__":
-    print("main module")
+    pass
 
+
+colors = ['black','white']
+sizes = ['S','M','L']
+
+shirts = [(color,size) for color in colors for size in sizes if color == 'white']
+
+print(shirts)
--- LearnPython/Samples/comprehensionEx.py
+++ LearnPython/Samples/comprehensionEx.py
@@ -1,6 +1,6 @@
 #list comprehension
 #[expression for item in iterable if condition]
-
+#[]里的list comprehension的变量有自己的作用域
 [number for number in range(1,6) if number%2 == 0]
 
 
@@ -9,3 +9,12 @@
 
 #set comprehension
 #{expression for item in iterable}
+
+colors = ['black','white']
+sizes = ['S','M','L']
+
+shirts = [(color,size) for color in colors for size in sizes if color == 'white']
+
+print(shirts)
+
+#generator expression 是使用listcomps一样的语法，但是用()而不是[]，它是迭代器模式，每次生成一个值
