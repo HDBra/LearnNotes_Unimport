05d0bf7023089986889580f4307526008a678375
luchunminglu <luchunminglu@gmail.com>
2016-06-28 11:36:24 +0800

    cc

---
+++ 通用工具/DemoBrowser_Webkit/CefSharpDemo/MainForm.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using CefSharp;
+using CefSharp.WinForms;
+
+namespace CefSharpDemo
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+
+            Cef.Initialize();
+            ChromiumWebBrowser browser = new ChromiumWebBrowser("http://ie.icoa.cn/");
+            browser.Dock = DockStyle.Fill;
+            //browser.SetZoomLevel(0.5);
+            this.Controls.Add(browser);
+        }
+    }
+}
--- 通用工具/DemoBrowser_Webkit/DemoBrowser/ReadMe.txt
+++ 通用工具/DemoBrowser_Webkit/DemoBrowser/ReadMe.txt
@@ -1,6 +1,7 @@
 ﻿http://www.codeproject.com/Tips/825526/Csharp-WebBrowser-vs-Gecko-vs-Awesomium-vs-OpenWeb
 
-Awesomium 
-EO.WebBrowser （需破解）
-两者是webkit内核的，已经测试EO.WebBrowser
+Awesomium （商业软件）
+EO.WebBrowser （商业软件）
+CefSharp (开源的最好的)
+两者是webkit内核的，本程序使用EO.WebBrowser
 http://wiki.awesomium.net/wpf/webcontrol.html
