9edb583b5c78a616e91ee1dfc6ea55092682563f
luchunminglu <luchunminglu@gmail.com>
2016-07-18 11:26:15 +0800

    dd

--- LearnPython/PythonUtils.py
+++ LearnPython/PythonUtils.py
@@ -1,8 +1,15 @@
 import sys
 
+# python中字符串是不可变的， python所有一切都是对象， python所有的变量名可以看成void *,  python中 string tuple number是不可更改的，每次操作实际上创建一个新的对象
+# python传值或者传参可以认为是void*传值
+
 if __name__ == "__main__":
-    #第一个参数是文件路径本身，接下来是具体参数
+    # 第一个参数是文件路径本身，接下来是具体参数
     print(sys.argv[0])
 
+def add(i):
+    i = [10]
 
-import Samples.slicesEx
+a = []
+add(a)
+print(a)
