b9e52c0a2f1bbe3c0a53e9a50422f686d4f9b98e
luchunminglu <luchunminglu@gmail.com>
2016-08-23 10:50:53 +0800

    cc

---
+++ NoUI/NoUI/MainWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace NoUI
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// 初始化
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void MainWindow_OnInitialized(object sender, EventArgs e)
+        {
+            this.WindowStartupLocation = WindowStartupLocation.Manual;
+            this.Left = -1000;
+            this.Top = -1000;
+        }
+    }
+}
---
+++ NoUI/NoUI/ReadMe.txt
@@ -0,0 +1,8 @@
+﻿将一个wpf程序设置为不显示：
+Width = Height = MaxHeight = MaxWidth = 0;
+WidowStyle = None//不显示最大最小化按钮
+ShowInTaskbar = false //在任务栏和任务管理器应用程序列表中不显示
+this.WindowState = Minimized//最小化显示
+this.WindowStartupLocation = WindowStartupLocation.Manual;
+this.Left = -1000;//设置显示位置
+this.Top = -1000;
