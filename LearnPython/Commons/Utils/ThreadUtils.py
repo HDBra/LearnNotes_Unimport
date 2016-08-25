c366e038016739fd9fd799ac291adcf7ef170676
luchunminglu <luchunminglu@gmail.com>
2016-08-25 22:34:10 +0800

    cc

--- LearnPython/Commons/Utils/ThreadUtils.py
+++ LearnPython/Commons/Utils/ThreadUtils.py
@@ -2,6 +2,7 @@
 
 import threading
 import queue
+import _thread
 
 # 由于GIL的存在，即python解释器进程同时只能有一个线程执行，多线程不能提高cpu限制的性能，可以提高IO限制的性能
 
@@ -25,6 +26,13 @@ def threadCall(action,args):
     :param args:
     :return:
     '''
-    thequeue = queue.Queue()
+    thequeue = queue.Queue() #多线程安全的队列
     thread = threading.Thread(target=action,args=(thequeue,))
     thread.start()
+
+def allocLock():
+    '''
+    分配锁
+    :return:
+    '''
+    lock = _thread.allocate_lock()
