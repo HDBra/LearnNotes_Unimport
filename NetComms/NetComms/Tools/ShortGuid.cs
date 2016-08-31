88104ea2e504ba6dcf19390aa0ac5d89cc2d20c4
luchunminglu <luchunminglu@gmail.com>
2016-08-31 14:59:54 +0800

    cc

---
+++ NetComms/NetComms/Tools/ShortGuid.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetComms.Tools
+{
+    /// <summary>
+    /// 表示一个短的全局唯一标识符
+    /// 用于标记一个连接点
+    /// </summary>
+    public struct ShortGuid
+    {
+
+
+
+
+        #region 构造函数
+
+        #endregion
+
+    }
+}
