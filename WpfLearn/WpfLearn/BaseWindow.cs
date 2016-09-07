746610bac02134d1626f02dc47e807bdb6eebd1b
luchunminglu <luchunminglu@gmail.com>
2016-09-07 21:33:26 +0800

    cc

--- WpfLearn/WpfLearn/BaseWindow.cs
+++ WpfLearn/WpfLearn/BaseWindow.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
+using System.Windows.Threading;
 
 namespace WpfLearn
 {
@@ -17,7 +18,40 @@ namespace WpfLearn
         /// </summary>
         public BaseWindow()
         {
-            
+
         }
+
+        /// <summary>
+        /// 多线程执行
+        /// </summary>
+        /// <param name="uiAction">回调到ui线程的内容</param>
+        public void SafeDispatch(Action uiAction)
+        {
+            //立刻返回，同时把action放到dispatcher的队列中
+            //invoke是等待action在dispatcher队列中执行完毕后返回
+            Dispatcher.BeginInvoke(new Action(() =>
+            {
+                if (this.IsLoaded && uiAction!=null)
+                {
+                    uiAction();
+                }
+            }),DispatcherPriority.Normal);
+        }
+
+        /// <summary>
+        /// 在多线程中执行一个action
+        /// </summary>
+        /// <param name="nonUiAction"></param>
+        public void RunTask(Action nonUiAction)
+        {
+            Task.Factory.StartNew(() =>
+            {
+                if (nonUiAction != null)
+                {
+                    nonUiAction();
+                }
+            });
+        }
+
     }
 }
