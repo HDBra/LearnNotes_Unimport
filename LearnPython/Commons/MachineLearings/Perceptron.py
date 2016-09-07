ae444345c1c762c338e8181778ef3a0532ea4400
luchunminglu <luchunminglu@gmail.com>
2016-09-07 21:12:17 +0800

    cc

--- LearnPython/Commons/MachineLearings/Perceptron.py
+++ LearnPython/Commons/MachineLearings/Perceptron.py
@@ -4,6 +4,133 @@ import numpy
 class Perceptron:
     '''
     感知器类型
+    eta: 学习速率
+    n_iter:迭代次数
     '''
+
+    def __init__(self,eta=0.01,n_iter=10):
+        self.eta = eta
+        self.n_iter = n_iter
+
+
+
+    def train(self,x,y):
+        '''
+        训练数据
+        :param x: x
+        :param y: x对应的y目标值
+        :return:
+        '''
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
---
+++ WpfLearn/WpfLearn/BaseWindow.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace WpfLearn
+{
+    /// <summary>
+    /// wpf基类Window
+    /// </summary>
+    public class BaseWindow:Window
+    {
+        /// <summary>
+        /// 构造函数
+        /// </summary>
+        public BaseWindow()
+        {
+            
+        }
+    }
+}
